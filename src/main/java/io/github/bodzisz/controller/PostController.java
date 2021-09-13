e3bcb9b5fa1000cc8fb65234affc6b2b2d3e30be
Bodzisz <kacpiw01pol@gmail.com>
2021-09-13 18:20:43 +0200

    Add contoller

--- src/main/java/io/github/bodzisz/controller/PostController.java
+++ src/main/java/io/github/bodzisz/controller/PostController.java
@@ -1,2 +1,33 @@
-package io.github.bodzisz.controller;public class PostController {
+package io.github.bodzisz.controller;
+
+import io.github.bodzisz.enitity.Post;
+import io.github.bodzisz.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/posts")
+public class PostController {
+    private final PostService postService;
+
+    public PostController(final PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public String showPosts(Model model) {
+        model.addAttribute("posts", postService.findAll());
+        return "post-list";
+    }
+
+    @GetMapping("/{id}")
+    public String showSinglePost(@PathVariable("id") int id, Model model) {
+        model.addAttribute("post", postService.findById(id));
+        return "single-post";
+    }
 }
--- src/main/java/io/github/bodzisz/controller/PostRestController.java
+++ src/main/java/io/github/bodzisz/controller/PostRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("/api/posts")
 public class PostRestController {
     private final PostService postService;
 
