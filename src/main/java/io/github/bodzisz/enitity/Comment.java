a34484e3fc9e9951aee4b8d80d22375933b8c149
Bodzisz <kacpiw01pol@gmail.com>
2021-09-13 18:18:30 +0200

    Add enitites

--- src/main/java/io/github/bodzisz/enitity/Comment.java
+++ src/main/java/io/github/bodzisz/enitity/Comment.java
@@ -1,2 +1,32 @@
-package io.github.bodzisz.enitity;public class Comment {
+package io.github.bodzisz.enitity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String content;
+
+    public Comment() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
--- src/main/java/io/github/bodzisz/enitity/Post.java
+++ src/main/java/io/github/bodzisz/enitity/Post.java
@@ -1,2 +1,53 @@
-package io.github.bodzisz.enitity;public class Post {
+package io.github.bodzisz.enitity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String Title;
+    private String content;
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "post_id")
+    private List<Comment> comments;
+
+    public Post() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
 }
