ea5908b45cd07840eaa10fb2080384e23d02b6d5
Bodzisz <kacpiw01pol@gmail.com>
2021-09-13 18:20:13 +0200

    Add flyway - creating tables

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE IF NOT EXISTS posts (
+    id int NOT NULL auto_increment,
+    title varchar(50) NOT NULL,
+    content varchar(10000) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+    id int NOT NULL auto_increment,
+    content varchar(5000),
+    post_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (post_id) REFERENCES posts(id)
+);
