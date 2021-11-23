a06c1e3fd757e44b3a64a6f9c649bfbd23293397
Bodzisz <kacpiw01pol@gmail.com>
2021-11-23 02:00:14 +0100

    Add parameter constructor

--- src/main/java/io/github/bodzisz/enitity/User.java
+++ src/main/java/io/github/bodzisz/enitity/User.java
@@ -42,6 +42,13 @@ public class User {
         this.password = userWriteModel.getPassword();
     }
 
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.enabled = 1;
+    }
+
 
     public int getId() {
         return id;
